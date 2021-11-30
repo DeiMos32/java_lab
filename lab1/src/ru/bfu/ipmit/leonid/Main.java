105a810df0cd8e0ab63a90029e57f88ed767d0ce
DeiMos32 <72670282+DeiMos32@users.noreply.github.com>
2021-11-30 22:37:36 +0200

    Update Main.java

--- lab1/src/ru/bfu/ipmit/leonid/Main.java
+++ lab1/src/ru/bfu/ipmit/leonid/Main.java
@@ -6,7 +6,7 @@ import java.nio.charset.StandardCharsets;
 
 public class Main {
     public static void main(String[] args) {
-        String greeting = "Hello, Владислав!";
+        String greeting = "Hello, Leonid!";
         String sha256hex = Hashing.sha256()
                 .hashString(greeting, StandardCharsets.UTF_8)
                 .toString();
